54713fb48efb7a8a0ec821971e75c590410a09bf
Your Name <you@example.com>
2016-10-04 11:39:39 +0200

    Angular and Firebase App

--- batch-server.ts
+++ batch-server.ts
@@ -1,11 +1,26 @@
 
-
 import {firebaseConfig} from "./src/environments/firebase.config";
-import {initializeApp} from 'firebase';
+import {initializeApp, auth} from 'firebase';
 
 console.log('Running batch server ...');
 
+initializeApp(firebaseConfig);
 
-var app = initializeApp(firebaseConfig);
+
+auth()
+    .signInWithEmailAndPassword('admin@angular-university.io', 'test123')
+    .then(runConsumer)
+    .catch(onError);
+
+function onError(err) {
+    console.error("Could not login", err);
+    process.exit();
+}
+
+function runConsumer() {
+    console.log("Running consumer ...");
+}
+
+
 
 
