8232c5e5f81c9358061a3691cdaf0dce5c739e7c
Your Name <you@example.com>
2016-10-04 11:57:50 +0200

    Angular and Firebase App

--- batch-server.ts
+++ batch-server.ts
@@ -1,6 +1,7 @@
 
 import {firebaseConfig} from "./src/environments/firebase.config";
-import {initializeApp, auth} from 'firebase';
+import {initializeApp, auth,database} from 'firebase';
+
 
 console.log('Running batch server ...');
 
@@ -19,6 +20,35 @@ function onError(err) {
 
 function runConsumer() {
     console.log("Running consumer ...");
+
+    var Queue = require('firebase-queue');
+    var queueRef = database().ref('queue');
+
+    var lessonsRef = database().ref("lessons");
+    var lessonsPerCourseRef = database().ref("lessonsPerCourse");
+
+
+    var queue = new Queue(queueRef, function(data, progress, resolve, reject) {
+
+        console.log('received delete request ...',data);
+
+        const deleteLessonPromise = lessonsRef.child(data.lessonId).remove();
+
+        const deleteLessonPerCourseRef = lessonsPerCourseRef.child(data.courseId + '/' + data.lessonId).remove();
+
+        Promise.all([deleteLessonPromise, deleteLessonPerCourseRef])
+            .then(
+                function() {
+                    console.log("lesson deleted");
+                    resolve();
+                }
+            ).catch(function() {
+            console.log("lesson deletion in error");
+            reject();
+        });
+
+    });
+
 }
 
 
